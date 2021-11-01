182dceb70516eed438877b296f75b82c40430499
marvin <marvin@thismarvin.com>
2021-11-01 15:48:59 -0500

    feat: laid out basic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,61 @@
 mod utils;
 
+use bitflags::bitflags;
 use wasm_bindgen::prelude::*;
+
+const BOARD_WIDTH: usize = 8;
+const BOARD_HEIGHT: usize = 8;
+
+enum Color {
+    White,
+    Black,
+}
+
+enum PieceType {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+
+enum Piece {
+    Color(PieceType),
+}
+
+// TODO(thismarvin): Is there a more idiomatic approach to this?
+bitflags! {
+    struct CastlingAbility : u32 {
+        const WHITE_KING_SIDE = 1 << 0;
+        const WHITE_QUEEN_SIDE = 1 << 1;
+        const BLACK_KING_SIDE = 1 << 2;
+        const BLACK_QUEEN_SIDE = 1 << 3;
+    }
+}
+
+struct Coordinate {
+    index: usize,
+}
+
+struct LAN {
+    start: Coordinate,
+    end: Coordinate,
+    promotion: Option<PieceType>,
+}
+
+struct FEN<'a> {
+    placement: &'a str,
+    side_to_move: Color,
+    castling_ability: Option<CastlingAbility>,
+    en_passant_target: Option<Coordinate>,
+    half_moves: usize,
+    full_moves: usize,
+}
+
+struct Board {
+    pieces: [Piece; BOARD_WIDTH * BOARD_HEIGHT],
+}
 
 #[wasm_bindgen]
 extern "C" {
