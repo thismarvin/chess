966015032708c99f4a8693cef4ade611e2f6c1bd
marvin <marvin@thismarvin.com>
2021-11-18 21:58:00 -0600

    chore: setup criterion.rs

---
+++ benches/perft.rs
@@ -0,0 +1,18 @@
+use chess;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn kiwipete(depth: u8) -> Result<u128, chess::ChessError> {
+    let fen = chess::Fen::try_from(
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+    )?;
+    let state = chess::State::from(fen);
+
+    chess::Engine::perft(&state, depth)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("perft_kiwipete", |b| b.iter(|| kiwipete(black_box(3))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
